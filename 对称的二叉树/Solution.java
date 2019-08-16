e478e5b4a2eaba9d1504a37b2f7d52793986705b
easylifegoal <easylifegoal@outlook.com>
2019-08-16 16:37:18 +0800

    21

---
+++ 对称的二叉树/Solution.java
@@ -0,0 +1,28 @@
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    boolean isSymmetrical(TreeNode pRoot) {
+
+        return helper(pRoot, pRoot);
+    }
+
+    boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+
+        return helper(left.left, right.right)
+            && helper(left.right, right.left);
+    }
+}
